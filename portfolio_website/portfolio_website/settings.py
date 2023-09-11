8d7fe8ce72809f7dc2697c1f7b5139d19d5d1306
panosker <panagiotiskeramisanos@gmail.com>
2023-09-11 06:33:36 -0700

    collect static

--- portfolio_website/portfolio_website/settings.py
+++ portfolio_website/portfolio_website/settings.py
@@ -4,8 +4,6 @@ import dj_database_url
 from decouple import config
 
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -18,9 +16,9 @@ SECRET_KEY = config("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -49,7 +47,7 @@ ROOT_URLCONF = "portfolio_website.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'portfolio_website/app/templates')],
+        "DIRS": [os.path.join(BASE_DIR, "portfolio_website/app/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -68,8 +66,8 @@ WSGI_APPLICATION = "portfolio_website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASE_URL = config('DATABASE_URL')
-DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
+DATABASE_URL = config("DATABASE_URL")
+DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
 
 
 # Password validation
@@ -106,11 +104,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "portfolio_website/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "portfolio_website/static")
 
-MEDIA_URL = "/images/"
+MEDIA_URL = "media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "portfolio_website/static/images")
 
